eb8b0980813088fc50180a0cb43b151052fe4569
Dawid Poradzinski <dawid.poradzinski@gmail.com>
2024-02-03 03:15:02 +0100

    First change. Added version, description

---
+++ src/main/java/net/dayid/tutorial/TutorialClient.java
@@ -0,0 +1,12 @@
+package net.dayid.tutorial;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TutorialClient implements ClientModInitializer{
+
+    @Override
+    public void onInitializeClient() {
+        
+    }
+    
+}
